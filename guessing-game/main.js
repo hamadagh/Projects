a8fb8f2179b1c8161ffc68a47b11328a75cb5803
Ahmad <hamadagh777@gmail.com>
2019-07-10 13:20:13 +0200

    10th of july

--- guessing-game/main.js
+++
@@ -1,3 +0,0 @@
-let randomNumber = Math.floor(Math.random()* 10 + 1);
-console.log(randomNumber);
-
--- quoteofday/main.js
+++
@@ -1,6 +0,0 @@
-function random (items){
-   let item = items[Math.floor(Math.random()*items.length)];
-    document.getElementById('quotes').innerHTML = item;
-
-}
-export {random};
