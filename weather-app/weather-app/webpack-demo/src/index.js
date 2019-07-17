be36db9fc44e16aeb7f261ce617384f177824ae5
Ahmad <hamadagh777@gmail.com>
2019-07-17 14:48:16 +0200

    17th of july

--- weather-app/weather-app/webpack-demo/src/index.js
+++ weather-app/weather-app/webpack-demo/src/index.js
@@ -1,10 +1,48 @@
 import _ from 'lodash';
-import {getCity} from "./token";
+import {getCity, getWeather} from "./token";
 import "./banner.css";
+import {images} from './banner';
 
 
 
 
-getCity('berlin')
-.then(data => console.log(data))
+getCity('berlin').then(data => {
+  return getWeather(data.Key);
+}).then(data => {
+ console.log(data); 
+})
 .catch(err => console.log(err));
+
+
+const updateCity = async (city) => {
+  let cityDetails = await getCity(city);
+  console.log(cityDetails.Key);
+  let weather = await getWeather(cityDetails.Key);
+ 
+  
+  return ({
+      cityDetails : cityDetails,
+      weather : weather
+ });
+
+}
+const cityInput = document.querySelector('.input');
+const cityForm = document.querySelector('#form'); 
+
+cityForm.addEventListener('submit', e => {
+    e.preventDefault();
+    let city = cityInput.value.trim();
+    console.log(city);
+     cityInput.value = '';
+    updateCity(city).then(data => console.log(data))
+    .catch(err => console.log(err));
+
+    document.querySelector('#header').innerHTML = `${city}`;
+    document.querySelector('#temperature').innerHTML = ``;
+
+
+})
+
+images();
+
+
--- weather-app/weather-app/webpack-demo/src/token.js
+++ weather-app/weather-app/webpack-demo/src/token.js
@@ -1,6 +1,13 @@
 const apiKey = "GMUlOH1VSDULkcAgUcCkxmUp4LGbLtUt";
 
+const getWeather = async (locationKey) => {
+    let base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    let query = `${locationKey}?apikey=${apiKey}`;
 
+    let response = await fetch(base + query);
+    let data = await response.json();
+    return data[0];
+}
 const getCity = async (city) => {
     let base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     let query = `?apikey=${apiKey}&q=${city}`;
@@ -11,6 +18,4 @@ const getCity = async (city) => {
   }
 
 
-
-
-export {getCity};
+export {getCity, getWeather, apiKey};
