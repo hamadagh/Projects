2d4873f4e483b48c345c774b8de80037ace7adff
Ahmad <hamadagh777@gmail.com>
2019-07-17 23:48:56 +0200

    third commit

--- weather-app/weather-app/webpack-demo/src/index.js
+++ weather-app/weather-app/webpack-demo/src/index.js
@@ -3,8 +3,23 @@ import {getCity, getWeather} from "./token";
 import "./banner.css";
 import {images} from './banner';
 
+// rick eyes
 
+$(".move-area").mousemove(function(event) {
+  var eye = $(".eye");
+  var x = (eye.offset().left) + (eye.width() / 2);
+  var y = (eye.offset().top) + (eye.height() / 2);
+  var rad = Math.atan2(event.pageX - x, event.pageY - y);
+  var rot = (rad * (180 / Math.PI) * -1) + 180;
+  eye.css({
+    '-webkit-transform': 'rotate(' + rot + 'deg)',
+    '-moz-transform': 'rotate(' + rot + 'deg)',
+    '-ms-transform': 'rotate(' + rot + 'deg)',
+    'transform': 'rotate(' + rot + 'deg)'
+  });
+});
 
+// API request
 
 getCity('berlin').then(data => {
   return getWeather(data.Key);
@@ -35,8 +50,13 @@ cityForm.addEventListener('submit', e => {
     console.log(city);
      cityInput.value = '';
     updateCity(city).then(data => {
+      // background image
       document.querySelector('#header').innerHTML = `${city}`;
       document.querySelector('#temperature').innerHTML = Math.floor(`${data.weather.Temperature.Metric.Value}`)+ "&#8451;";
+      // icons
+     let icon = document.querySelector('.icon img');
+      let icons = `../src/icons/${data.weather.WeatherIcon}.svg`;
+      icon.setAttribute('src', icons );
       console.log(data)})
     .catch(err => console.log(err));
 
@@ -44,6 +64,7 @@ cityForm.addEventListener('submit', e => {
 
 
 })
+
 
 images();
 
