c8f2bc608a99a8000b2c8d07b2b9de1194cfe80b
Ahmad <hamadagh777@gmail.com>
2019-08-19 22:56:59 +0200

    adding navbar

--- react-blog/react-blog/src/App.js
+++ react-blog/react-blog/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import NavigationBar from './component/NavigationBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-   
-      
+     <NavigationBar />
     </div>
   );
 }
---
+++ react-blog/react-blog/src/component/NavigationBar.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {Navbar, Nav} from 'react-bootstrap';
+
+export class NavigationBar extends Component {
+    render() {
+        return (
+            <div>
+            <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+  </Navbar>  
+            </div>
+        )
+    }
+}
+
+export default NavigationBar
