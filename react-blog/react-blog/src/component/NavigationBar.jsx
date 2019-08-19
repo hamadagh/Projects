d066c2f0f10408894548a1075a9b1f1c7f89f9a7
Ahmad <hamadagh777@gmail.com>
2019-08-19 23:27:03 +0200

    updating navbar

--- react-blog/react-blog/src/component/NavigationBar.jsx
+++ react-blog/react-blog/src/component/NavigationBar.jsx
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
-import {Navbar, Nav} from 'react-bootstrap';
+import React from 'react';
 
-export class NavigationBar extends Component {
-    render() {
-        return (
-            <div>
-            <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-  </Navbar>  
-            </div>
-        )
-    }
+const NavigationBar = () => {
+    return(
+        <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="/Home">Navbar</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav text-center">
+      <li className="nav-item active">
+        <a className="nav-link" href="/Home">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/CreatePost">Create post</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/ShowPost">Show Post</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+        </div>
+    )
 }
 
-export default NavigationBar
+export default NavigationBar;
