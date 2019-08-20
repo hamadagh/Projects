71f54530033883b76f6690f49009dc7fe3699a4a
Ahmad <hamadagh777@gmail.com>
2019-08-20 14:46:05 +0200

    navbar updated

--- react-blog/react-blog/src/component/NavBar.jsx
+++ react-blog/react-blog/src/component/NavBar.jsx
@@ -4,16 +4,16 @@ const NavBar = () => {
     return(
         <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark pl-5">
-  <a className="navbar-brand" href="/Home">Navbar</a>
+  <a className="navbar-brand logo" href="/Home">Blog</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav text-center">
-      <li className="nav-item active">
+      <li className="nav-item active mr-5">
         <a className="nav-link" href="/Home">Home <span className="sr-only">(current)</span></a>
       </li>
-      <li className="nav-item">
+      <li className="nav-item mr-5">
         <a className="nav-link" href="/CreatePost">Create post</a>
       </li>
       <li className="nav-item">
