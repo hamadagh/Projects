beb334e4a825ac77406d8f144f723ea65452dd7e
Ahmad <hamadagh777@gmail.com>
2019-09-03 15:01:56 +0200

    updating  svg file

--- react-blog/react-blog/src/component/Home.jsx
+++ react-blog/react-blog/src/component/Home.jsx
@@ -8,12 +8,11 @@ export class Home extends Component {
             <div>
                 <h1 className='text-center mt-5 shadows'>A Platform For Sharing Knowledge</h1>
                 <h1 className='text-center shadows'>And <span className='IT'>IT</span> Solution</h1>
-                <div>
-                    <h2 className='home-header text-center'><span className='it'>T</span><span className='it1'>B</span></h2>
-                </div>
+              
                 <div className='isometric d-flex justify-content-center'>
-                </div>
                 <Isometric/>
+                </div>
+                
             </div>
         )
     }
